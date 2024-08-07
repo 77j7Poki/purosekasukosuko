329871d1f69312da518fb39d947ea4269863154f
goki <goki@MacBook-Air-7.local>
2024-08-08 02:51:49 +0900

    APIとデータベース

--- newprosekafile/api/count.js
+++
@@ -1,9 +0,0 @@
-const counts = [0, 0, 0, 0]; // グローバルスコープで一時的に保持
-
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-        return res.json({ counts });
-    } else {
-        return res.status(405).send('Method Not Allowed');
-    }
-}
